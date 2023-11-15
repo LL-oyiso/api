8bf6de7d07f37ce7a59ea94b601e3b6fa0122f71
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-15 21:47:43 +0200

    packages

---
+++ index.js
@@ -0,0 +1,9 @@
+// declare variable for app which represents actuall api we are building 
+const app = require('express')();
+const PORT = 8080;
+
+// listen on specific port
+app.listen(
+    PORT,
+    () => console.log('its alive on http://localhost:${PORT}')
+)
